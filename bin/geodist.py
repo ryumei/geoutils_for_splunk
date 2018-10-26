948753af27edaad366310642fc877a9ec52fca2c
NAKAJIMA Takaaki <takaaki.nakajima@kestrel.isa-geek.net>
2018-10-26 16:57:14 +0900

    configurable fieldnames

--- bin/geodist.py
+++ bin/geodist.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from splunklib.searchcommands import dispatch, StreamingCommand, Configuration
+from splunklib.searchcommands import dispatch, StreamingCommand, Configuration, Option, validators
 from geopy.distance import geodesic
 
 @Configuration()
 class GeodistCommand(StreamingCommand):
+    src_lat_field = Option(require=False, default='start_station_latitude')
+    src_lon_field = Option(require=False, default='start_station_longitude')
+    dst_lat_field = Option(require=False, default='end_station_latitude')
+    dst_lon_field = Option(require=False, default='end_station_longitude')
+    dist_field = Option(require=False, default='geodist')
+
     def stream(self, records):
-        src_lat_field = 'start_station_latitude'
-        src_lon_field = 'start_station_longitude'
-        dst_lat_field = 'end_station_latitude'
-        dst_lon_field = 'end_station_longitude'
-        dist_field = 'geodist'
         for record in records:
-            src_point = (record[src_lat_field], record[src_lon_field])
-            dst_point = (record[dst_lat_field], record[dst_lon_field])
-            record[dist_field] = geodesic(src_point, dst_point).km
+            src_point = (record[self.src_lat_field], record[self.src_lon_field])
+            dst_point = (record[self.dst_lat_field], record[self.dst_lon_field])
+            record[self.dist_field] = geodesic(src_point, dst_point).km
             yield record
 
 if __name__ == "__main__":
